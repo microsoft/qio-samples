e5aa4c7b87f691af5547712b613cf776c68b27d7
Anita Ramanan <anraman@microsoft.com>
2022-06-23 08:17:42 -0700

    Updated Toshiba sample with bSB dSB options

--- samples/getting-started/toshiba-sqbm/toshiba-sqbm.py
+++ samples/getting-started/toshiba-sqbm/toshiba-sqbm.py
@@ -3,12 +3,12 @@
 
 from azure.quantum import Workspace
 from azure.quantum.optimization import Problem, ProblemType, Term
-from azure.quantum.optimization.toshiba import SimulatedBifurcationMachine
+from azure.quantum.target.toshiba import SimulatedBifurcationMachine
 
 # Copy the settings for your workspace below
 workspace = Workspace(
-    resource_id = "",   # add the Resource ID of the Azure Quantum workspace you created with the Toshiba provider
-    location = ""
+    resource_id = "/subscriptions/b5c28c80-889c-4919-8e6b-3569c950646e/resourceGroups/AQDW3-new/providers/Microsoft.Quantum/Workspaces/AQDW-workspace-new",   # add the Resource ID of the Azure Quantum workspace you created with the Toshiba provider
+    location = "westus2"
 )
 
 # Define the problem
@@ -24,11 +24,50 @@ problem.add_terms([
     Term(w=4, indices=[3,2])
 ])
 
-# Create Toshiba SQBM+ solver
+# Create Toshiba SQBM+ solver with default parameters
 solver = SimulatedBifurcationMachine(workspace)
 
 # Submit the problem for solving with Toshiba SQBM+
+print("Submitting first problem (default params):")
 result = solver.optimize(problem)
 
 # Print out the result
-print(result)
+print("result")
+
+# Create Toshiba SQBM+ solver, specifically choosing to use the high-speed
+# Ballistic Simulated Bifurcation algorithm (bSB), designed to find a good solution in a short time
+# This is the default algorithm chosen if no 'algo' parameter is provided
+solver = SimulatedBifurcationMachine(workspace, algo="1.5")
+
+# Rename problem for easy differentiation later
+problem.name = "bSB algorithm test problem"
+
+# Submit the problem 
+print("\nSubmitting bSB problem:")
+result = solver.optimize(problem)
+print("result")
+
+# Create Toshiba SQBM+ solver, specifically choosing to use the high-accuracy 
+# Discrete Simulated Bifurcation algorithm (dSB) which finds more accurate solutions
+# at a calculation speed that surpasses that of other machines (https://www.science.org/doi/10.1126/sciadv.abe7953)
+solver = SimulatedBifurcationMachine(workspace, algo="2.0")
+
+# Rename problem for easy differentiation later
+problem.name = "dSB algorithm test problem"
+
+# Submit the problem 
+print("\nSubmitting dSB problem:")
+result = solver.optimize(problem)
+print("result")
+
+# Create Toshiba SQBM+ solver, making use of the auto-tune function provided
+# which will select the best algorithm and parameters to use based on problem characteristics
+solver = SimulatedBifurcationMachine(workspace, auto=True)
+
+# Rename problem for easy differentiation later
+problem.name = "auto-select algorithm test problem"
+
+# Submit the problem 
+print("\nSubmitting auto-select problem:")
+result = solver.optimize(problem)
+print("result")
